b23f082ff3158626bd47fc1ef82f31459d0556f9
markshawn2020 <shawninjuly@gmail.com>
2025-05-27 11:30:29 +0800

    add test mode

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,10 +1,23 @@
 import { Component, PropsWithChildren } from 'react'
-import { View, Text, Button, Image } from '@tarojs/components'
+import { View, Text, Button, Image, Switch } from '@tarojs/components'
+import { AtButton, AtDivider } from 'taro-ui'
 import Taro from '@tarojs/taro'
 import { logger } from '../../services'
 import './index.scss'
 
-export default class Profile extends Component<PropsWithChildren> {
+interface IState {
+  isTestMode: boolean;
+  tapCount: number;
+}
+
+export default class Profile extends Component<PropsWithChildren, IState> {
+  constructor(props: PropsWithChildren) {
+    super(props);
+    this.state = {
+      isTestMode: false,
+      tapCount: 0
+    };
+  }
 
   componentDidMount() {
     Taro.setNavigationBarTitle({
@@ -20,12 +33,86 @@ export default class Profile extends Component<PropsWithChildren> {
       url: '/pages/invite/index'
     })
   }
+  
+  // 启用测试模式的隐藏触发器
+  handleTitleClick = () => {
+    const { tapCount } = this.state;
+    const newCount = tapCount + 1;
+    
+    if (newCount >= 5) {
+      // 连续点击5次标题，开启测试模式
+      logger.info('开启测试模式');
+      this.setState({ 
+        isTestMode: true,
+        tapCount: 0
+      });
+      Taro.showToast({
+        title: '测试模式已开启',
+        icon: 'success',
+        duration: 2000
+      });
+    } else {
+      this.setState({ tapCount: newCount });
+    }
+  }
+  
+  // 清除登录信息
+  handleClearLoginInfo = () => {
+    logger.info('清除登录信息');
+    Taro.removeStorageSync('token');
+    Taro.removeStorageSync('userInfo');
+    
+    Taro.showToast({
+      title: '登录信息已清除',
+      icon: 'success',
+      duration: 2000
+    });
+  }
+  
+  // 清除考试信息
+  handleClearExamInfo = () => {
+    logger.info('清除考试信息');
+    Taro.removeStorageSync('examInfo');
+    
+    Taro.showToast({
+      title: '考试信息已清除',
+      icon: 'success',
+      duration: 2000
+    });
+  }
+  
+  // 清除所有本地数据
+  handleClearAllData = () => {
+    logger.info('清除所有本地数据');
+    Taro.clearStorageSync();
+    
+    Taro.showToast({
+      title: '所有数据已清除',
+      icon: 'success',
+      duration: 2000
+    });
+  }
+  
+  // 关闭测试模式
+  handleCloseTestMode = () => {
+    logger.info('关闭测试模式');
+    this.setState({ isTestMode: false });
+    
+    Taro.showToast({
+      title: '测试模式已关闭',
+      icon: 'success',
+      duration: 2000
+    });
+  }
 
   render() {
+    const { isTestMode } = this.state;
+    
     return (
       <View className='profile-page'>
         <View className='page-content'>
-          <Text className='page-title'>👤 个人中心</Text>
+          {/* 通过连续点击标题5次激活测试模式 */}
+          <Text className='page-title' onClick={this.handleTitleClick}>👤 个人中心</Text>
           <Text className='page-desc'>管理个人信息和设置</Text>
           
           {/* 邀请好友CTA */}
@@ -48,6 +135,55 @@ export default class Profile extends Component<PropsWithChildren> {
           <View className='other-features'>
             <Text className='section-title'>常用功能</Text>
           </View>
+          
+          {/* 测试模式区域 - 仅在测试模式开启时显示 */}
+          {isTestMode && (
+            <View className='dev-options'>
+              <AtDivider content='开发者选项' fontColor='#FF6E26' lineColor='#FFE8E0' />
+              
+              <View className='dev-options-container'>
+                <AtButton 
+                  type='secondary' 
+                  size='small' 
+                  className='dev-button' 
+                  onClick={this.handleClearLoginInfo}
+                >
+                  清空登录信息
+                </AtButton>
+                
+                <AtButton 
+                  type='secondary' 
+                  size='small' 
+                  className='dev-button' 
+                  onClick={this.handleClearExamInfo}
+                >
+                  清空考试信息
+                </AtButton>
+                
+                <AtButton 
+                  type='primary' 
+                  size='small' 
+                  className='dev-button danger-button' 
+                  onClick={this.handleClearAllData}
+                >
+                  清空所有数据
+                </AtButton>
+                
+                <AtButton 
+                  type='secondary' 
+                  size='small' 
+                  className='dev-button close-button' 
+                  onClick={this.handleCloseTestMode}
+                >
+                  关闭测试模式
+                </AtButton>
+              </View>
+              
+              <View className='dev-note'>
+                <Text>注意：以上操作将立即生效且不可恢复</Text>
+              </View>
+            </View>
+          )}
         </View>
       </View>
     )
